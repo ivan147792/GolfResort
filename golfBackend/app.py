ffabb17070e9a0506d9cfbd0cdc2c8789223473e
Kizo1312 <toma.ilcic@gmail.com>
2025-06-17 13:14:03 +0200

    imamo flask database sa sqlite

--- golfBackend/app.py
+++ golfBackend/app.py
@@ -14,7 +14,8 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///golf.db"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 app.config["API_TITLE"] = "Golf Resort API"
